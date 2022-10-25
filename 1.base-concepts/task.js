234134f026030704aed25da49bfe7db4c3f2a955
anay333 <87089288+anay333@users.noreply.github.com>
2022-10-26 00:20:56 +0500

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,18 +1,24 @@
-"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  let d=b**2-4*a*c;
-  if (d>0) {
- arr=[(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)];
+    let arr;
+    let x;
+    let y;
+    let d = Math.pow(b, 2) - 4 * a * c;
+    if (d > 0) {
+      x = ((-b + Math.sqrt(d)) / (2 * a));
+      y = ((-b - Math.sqrt(d)) / (2 * a));
+      arr = [x, y];
+    } else if (d == 0) {
+      x = ((-b) / (2 * a));
+      arr = [x];
+    }
+    else if (d < 0) {
+      arr = [];
+  
+    } else {
+      console.log('Не существует такого квадратного уравнения')
+    }
+    return arr;// array
   }
-  if (d=0) {
-arr=-b/(2*a);
-  }
-  if (d<0) {
-arr=null;
-  }
-  return arr; // array
-}
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
