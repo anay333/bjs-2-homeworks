f7fc90c57c7055eef10c9947cf7346d183144c83
anay333 <87089288+anay333@users.noreply.github.com>
2022-10-26 14:02:08 +0500

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,42 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+min=arr[0];
+max=min;
+sum=0;
+arr.forEach(element => {
+  sum += element;
+  if (min >= element ) {min = element};
+  if (max <= element ) {max = element};
+});
+avg = Number((sum/arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum =0;
+  arr.forEach(element => {sum += element});
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+  let max =0;
+  arrOfArr.forEach(element => {
+    let tmp = func(element);
+    if (max <= tmp ) {max = tmp};
+  });
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max, d;
+  min = max = d = arr[0];
+  arr.forEach(element => {
+    if (min >= element ) {min = element};
+    if (max <= element ) {max = element};
+  });
+  d = max-min;
+  return Math.abs(d);
 }
